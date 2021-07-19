c924851cb911bf5cffa7b2bc41022e2be64c3c06
yoshlikmedia <yoshlikmedia@gmail.com>
2021-07-20 01:46:54 +0500

    Add requirements.txt and setup.py files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Cartoon-Picture-bot',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='yoshlikmedia',
+    author_email='yoshlikmedia@gmail.com',
+    description=''
+)
